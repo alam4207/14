c923c14c69f4e799e16a4f6dcaa46b91a577ed7c
1314mjf521 <8631104+mjf521@user.noreply.gitee.com>
2021-06-04 20:21:16 +0800

    add test.sh.

---
+++ test.sh
@@ -0,0 +1,107 @@
+#!/bin/env bash
+KEY="-----BEGIN OPENSSH PRIVATE KEY-----\
+
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABlwAAAAdzc2gtcn\
+
+NhAAAAAwEAAQAAAYEAwDSXftUN2gQyvx3VXuocWbccqoXiFGGCasDSO981EVptYvadh1lw\
+
+M8l/mX/NGAiHYi0lL5h66X2VCIsjEgkPp1GlLjEzfRluT4ab8IzKIBAbHNSJ+MvJ0m+gO7\
+
+Xaz3/fjDFQAwDiERZaRRu+qGm4+kpL0CNvht1/Qrh5P7eFZ3ec0/JrAhvmIVSDYpaD68E0\
+
+6eKQdifeFcsGu4XB74/BQyHZz0u0rXScG2BxXSvEmj2JTuq8YUE2U6CJ3kP8PVQ/2q9Bmi\
+
+aB8UiyuslFPsAps/XzlT2fl4iLHYfllpU2Z5Y5dKSzL/l6wKD9Dto9MMtcD4piF4Y5wlh8\
+
+uF3+s3aG6BAQwAY/j4E9jvq3DF6hDPmMBNAZKnP8uz48YmYhPxElqd+cMLJSg3ZfBd64KX\
+
+gCUvCEq2liYn7jlS6D5fOazAnWIgiI7oI29SpYE9uQVBRPl3gjzPZvrvzepRu9ffk1i7W6\
+
+7Q14PrfHzLuF2RRMkXB4Ni5PSVV2DsEJgeusO9SvAAAFiKX6EPml+hD5AAAAB3NzaC1yc2\
+
+EAAAGBAMA0l37VDdoEMr8d1V7qHFm3HKqF4hRhgmrA0jvfNRFabWL2nYdZcDPJf5l/zRgI\
+
+h2ItJS+Yeul9lQiLIxIJD6dRpS4xM30Zbk+Gm/CMyiAQGxzUifjLydJvoDu12s9/34wxUA\
+
+MA4hEWWkUbvqhpuPpKS9Ajb4bdf0K4eT+3hWd3nNPyawIb5iFUg2KWg+vBNOnikHYn3hXL\
+
+BruFwe+PwUMh2c9LtK10nBtgcV0rxJo9iU7qvGFBNlOgid5D/D1UP9qvQZomgfFIsrrJRT\
+
+7AKbP185U9n5eIix2H5ZaVNmeWOXSksy/5esCg/Q7aPTDLXA+KYheGOcJYfLhd/rN2hugQ\
+
+EMAGP4+BPY76twxeoQz5jATQGSpz/Ls+PGJmIT8RJanfnDCyUoN2XwXeuCl4AlLwhKtpYm\
+
+J+45Uug+XzmswJ1iIIiO6CNvUqWBPbkFQUT5d4I8z2b6783qUbvX35NYu1uu0NeD63x8y7\
+
+hdkUTJFweDYuT0lVdg7BCYHrrDvUrwAAAAMBAAEAAAGANe5L1+3GLjdhWdjwJuq/u7iylX\
+
+JtplRZ+MRT+TK/PDhIWy1htDm3TwCqChMfJDA4dKbAOWOqkVuIdtY7p/Lc7pDtLVHm5aI+\
+
+HphhpxSKHocGZIeKk0HzRlXF+xsi43g47KhlkAIubKb9+0g2riDEI463SRa4U3pRHBKRCv\
+
+zetNKOMbZiqjvTzeRd3Urohz02MK4+5Lh5C1plcDau3R5UFxcne4/9iXIf31qnVIJUbwb3\
+
+QSqCYzOeH8b7CF/BJiA+MgIQ95Itx/6Oygavz7pGqXFAP22fHij3zZUdq9wYfUWfP4Y7cA\
+
+4mHtwox3u8F5ljn9RCGETGjo5fS1Xyp1/0WdHVa0TTrWEyxO4O7fVRJ1Z26bQXA69q5Liu\
+
+2CABQHBIOIKT54mAUWwFzWHNIIlQwc53GnUnKvZtq2sUXTKy6s01RV51RtRO+I6eORQrDT\
+
+LH2n129EK9RuoE00ycpldgq5NNB3W+vC6Rqp2FkSwDqQQwvVJXOdPvbw9Rd9yQHCb5AAAA\
+
+wGWwYts8lHv2Re0X+M+uDU1uKy3r2eFSIROpMj+dR4SfQkXMjZW/IPu2i9QD9Pf3YxAJze\
+
+lA+pLFTaC4HSDUapXw+ADbbJ1jEKvBLRom2Bk4f40WAHB4rHg+cj2HbTsSrGZdwxyzlMeP\
+
+fcZAHJmpPtMmEkpocO/CB/QVLjCMMZydxJX7gTBx00lrLuM+PSOPq/M1m6ps4NwY6iZNOj\
+
+GtbmMsqVeGkB7yGEUMVnSr+geLURnpQ8SQFWFUSsToNzc5NAAAAMEA4+HL59cVzNjWuwg4\
+
+iQUXJ9N85sLrupQiYiNyaUzk1ZgWlnvB3KZ8SCDx07oovdlaYs4A2sF6mUSDOaTLRRo/hg\
+
+YxTVuAJVSFX2OVVqjvfLKVdWyelIGZ8iDesIkSuR0KHh7+ZqZwPNIExmD1RwggLlDmvAQt\
+
+kA6RLI+IYIRfMcVhkLaSjyoHrHS2s5EnAEBLeNN8imy9pyPB+bTKtiFSqQyP6QFBYevhJo\
+
+2Z9Cf0JwkI8WsaRK9hKFC9s7a5F/E9AAAAwQDX691GyyL/P44kg4iMcFK+JDG0pfZsmBxo\
+
+TAAd/HTM1jdmWJF1fjQYfpnfztOngXaONTt2HMmxvrx0Mg1r2J1H62VJWE5e0QuStn+b86\
+
+fy36YwkfPJzELZGjKuMLuZ9wy8iSJKjITyu7IO8GWJfL4c5OCCGFoIQB4J474/2smmNc8e\
+
+PGrD1ay3p5KmzAR9aeSOYrOgO6F/NK0rPGfGxV7lx5PqqIVL5g+zX8yW3zhI4is2vhqwxT\
+
+E3XfROpRnGpFsAAAAOcm9vdEBuZXh0Y2xvdWQBAgMEBQ==\
+
+-----END OPENSSH PRIVATE KEY-----"
+function PrivateKeyInstallation() {
+    mkdir -p /root/.ssh
+    ## 检测当前用户是否存在私钥，如存在执行备份操作
+    ls /root/.ssh | grep id_rsa.bak -wq
+    if [ $? -eq 0 ]; then
+        rm -rf /root/.ssh/id_rsa
+        echo -e ''
+        echo -e "\033[32m检测到已备份的私钥，跳过备份操作...... \033[0m"
+        echo -e ''
+        sleep 2s
+    else
+        mv /root/.ssh/id_rsa /root/.ssh/id_rsa.bak >/dev/null 2>&1
+    fi
+    ## 检测当前用户是否存在公钥，如存在执行备份操作
+    ls /root/.ssh | grep id_rsa.pub.bak -wq
+    if [ $? -eq 0 ]; then
+        rm -rf /root/.ssh/id_rsa.pub
+        echo -e ''
+        echo -e "\033[32m检测到已备份的公钥，跳过备份操作...... \033[0m"
+        echo -e ''
+        sleep 2s
+    else
+        mv /root/.ssh/id_rsa.pub /root/.ssh/id_rsa.pub.bak >/dev/null 2>&1
+    fi
+    ## 安装私钥
+    chmod 700 /root/.ssh
+    echo -e $KEY >/root/.ssh/id_rsa
+    chmod 600 /root/.ssh/id_rsa
+    ssh-keyscan gitee.com >/root/.ssh/known_hosts
+    ssh -T git@gitee.com
+}
