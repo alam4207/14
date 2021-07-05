d0afbd610d643e9e9cd24ee9a8268aac81fa10e2
1314mjf521 <8631104+mjf521@user.noreply.gitee.com>
2021-07-05 01:42:41 +0000

    update git_pull.sh.

--- git_pull.sh
+++ git_pull.sh
@@ -32,11 +32,11 @@ isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
 #docker部署的话使用下面这个地址
 #ScriptsURL=git@gitee.com:mjf521/jd_scrip.git
 #X86部署直接使用即可无需修改
-#ScriptsURL=git@gitee.com:mjf521/jd_scripts-1.git
+ScriptsURL=git@gitee.com:mjf521/jd_scripts-1.git
 #docker专用
 #ScriptsURL=git@gitee.com:mjf521/jd_scripts-2.git
 #实验
-ScriptsURL=git@gitee.com:mjf521/script-backup.git
+#ScriptsURL=git@gitee.com:mjf521/script-backup.git
 
 
 
