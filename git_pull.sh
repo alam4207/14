7f76e229d539e921f3bf7d9e67c1307a16f25f94
1314mjf521 <8631104+mjf521@user.noreply.gitee.com>
2021-07-11 11:58:57 +0000

    update git_pull.sh.

--- git_pull.sh
+++ git_pull.sh
@@ -28,11 +28,11 @@ ContentNewTask=${ShellDir}/new_task
 ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
 isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
-#ScriptsURL=https://gitee.com/mjf521/jd_scrip.git
+ScriptsURL=https://gitee.com/mjf521/jd_scrip.git
 #docker部署的话使用下面这个地址
 #ScriptsURL=git@gitee.com:mjf521/jd_scrip.git
 #X86部署直接使用即可无需修改
-ScriptsURL=git@gitee.com:mjf521/jd_scripts-1.git
+#ScriptsURL=git@gitee.com:mjf521/jd_scripts-1.git
 #docker专用
 #ScriptsURL=git@gitee.com:mjf521/jd_scripts-2.git
 #实验
